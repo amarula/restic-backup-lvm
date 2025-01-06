59fbcdc0c1bf8218c96569e2bee46900dbf573af
Michael Trimarchi <michael@amarulasolutions.com>
2025-01-06 19:31:46 +0100

    Remove duplicated key
    Change-Id: I4b00fcbdd12209691ae5b5a613b612ca62b754a3
    Signed-off-by: Michael Trimarchi <michael@amarulasolutions.com>

--- restic-lvm-tools.py
+++ restic-lvm-tools.py
@@ -30,7 +30,6 @@ RESTIC_PASSWORD = config.get('restic', 'password')
 env = os.environ
 
 extra_vars = {'AWS_ACCESS_KEY_ID': AWS_ACCESS_KEY_ID,
-              'AWS_ACCESS_KEY_ID': AWS_ACCESS_KEY_ID,
               'AWS_SECRET_ACCESS_KEY': AWS_SECRET_ACCESS_KEY,
               'AWS_DEFAULT_REGION' : AWS_DEFAULT_REGION,
               'BUCKET_NAME' : BUCKET_NAME,
